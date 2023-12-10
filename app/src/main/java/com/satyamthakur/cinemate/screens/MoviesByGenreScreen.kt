1df6fe26d4ee9cecd5955fa255c44cb4db7ce695
Satyam Thakur <sat.thakur20@gmail.com>
2023-12-10 17:49:57 +0530

    Customizing cards in New Released and Movies by Genre

--- app/src/main/java/com/satyamthakur/cinemate/screens/MoviesByGenreScreen.kt
+++ app/src/main/java/com/satyamthakur/cinemate/screens/MoviesByGenreScreen.kt
@@ -3,12 +3,15 @@ package com.satyamthakur.cinemate.screens
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -38,11 +41,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
+import com.satyamthakur.cinemate.R
 import com.satyamthakur.cinemate.models.MovieDetailsResponse
 import com.satyamthakur.cinemate.models.Result
 import com.satyamthakur.cinemate.ui.theme.poppinsFont
@@ -159,21 +166,69 @@ fun NormalMovieCardItem(
     movie: Result,
     navController: NavController
 ) {
-    Card(
-        modifier = Modifier.fillMaxWidth().height(280.dp),
-        shape = RoundedCornerShape(16.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.White
-        ),
-        elevation = CardDefaults.cardElevation(0.dp)
-    ) {
-        AsyncImage(
-            model = "https://image.tmdb.org/t/p/original${movie.poster_path}",
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize().clickable {
-                navController.navigate("movie_details/${movie.id}")
-            }
+    Column {
+        Card(
+            modifier = Modifier.fillMaxWidth().height(250.dp),
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            ),
+            elevation = CardDefaults.cardElevation(0.dp)
+        ) {
+            AsyncImage(
+                model = "https://image.tmdb.org/t/p/original${movie.poster_path}",
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize().clickable {
+                    navController.navigate("movie_details/${movie.id}")
+                }
+            )
+        }
+
+        Text(
+            text = movie.title,
+            fontFamily = poppinsFont,
+            fontWeight = FontWeight.Medium,
+            fontSize = 14.sp,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            color = Color.Black,
+            modifier = Modifier.padding(top = 8.dp)
         )
+
+
+        RatingStarsInGenre(movie.vote_average)
+
+    }
+
+}
+
+
+@Composable
+fun RatingStarsInGenre(votes: Double) {
+    var voteAverage = votes
+    voteAverage = voteAverage / 2
+    var fullCount = voteAverage.toInt()
+    voteAverage = voteAverage - fullCount.toDouble()
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        repeat(fullCount) {
+            Icon(
+                painter = painterResource(R.drawable.ic_star),
+                contentDescription = null,
+                tint = Color(0xffe8b923),
+                modifier = Modifier.padding(2.dp).size(16.dp)
+            )
+        }
+        if (voteAverage >= 0.5) {
+            Icon(
+                painter = painterResource(R.drawable.ic__star_half),
+                contentDescription = null,
+                tint = Color(0xffe8b923),
+                modifier = Modifier.padding(2.dp).size(18.dp)
+            )
+        }
     }
 }
--- app/src/main/java/com/satyamthakur/cinemate/screens/NewReleasedMovies.kt
+++ app/src/main/java/com/satyamthakur/cinemate/screens/NewReleasedMovies.kt
@@ -3,6 +3,7 @@ package com.satyamthakur.cinemate.screens
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,9 +11,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -26,10 +24,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.satyamthakur.cinemate.models.Result
+import com.satyamthakur.cinemate.ui.theme.bodyTextSB
+import com.satyamthakur.cinemate.ui.theme.poppinsFont
 import com.satyamthakur.cinemate.viewmodels.MoviesNowPlayingViewModel
 
 @Composable
@@ -64,7 +67,7 @@ fun ShowNowPlayingMovies (
             start = 16.dp,
             end = 16.dp
         ),
-        modifier = Modifier.padding(bottom = 24.dp, top = 8.dp)
+        modifier = Modifier.padding(bottom = 24.dp, top = 16.dp)
     ) {
         items(movies) { item ->
             MovieCardItemsNowPlaying(item, navigateToMovieDetails)
@@ -74,21 +77,37 @@ fun ShowNowPlayingMovies (
 
 @Composable
 fun MovieCardItemsNowPlaying(movie: Result, navigateToMovieDetails: (movieId: String) -> Unit) {
-    Card(
-        modifier = Modifier.width(120.dp).height(180.dp),
-        shape = RoundedCornerShape(16.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.White
-        ),
-        elevation = CardDefaults.cardElevation(0.dp)
+    Column(
+        modifier = Modifier.width(150.dp),
     ) {
-        AsyncImage(
-            model = "https://image.tmdb.org/t/p/original${movie.poster_path}",
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize().clickable {
-                navigateToMovieDetails(movie.id.toString())
-            }
+        Card(
+            modifier = Modifier.fillMaxWidth().height(200.dp),
+            shape = RoundedCornerShape(16.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            ),
+            elevation = CardDefaults.cardElevation(0.dp)
+        ) {
+            AsyncImage(
+                model = "https://image.tmdb.org/t/p/original${movie.poster_path}",
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize().clickable {
+                    navigateToMovieDetails(movie.id.toString())
+                }
+            )
+        }
+
+        Text(
+            text = movie.title,
+            fontFamily = poppinsFont,
+            fontWeight = FontWeight.Medium,
+            fontSize = 14.sp,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            color = Color.Black,
+            modifier = Modifier.padding(top = 8.dp)
         )
+
     }
 }
